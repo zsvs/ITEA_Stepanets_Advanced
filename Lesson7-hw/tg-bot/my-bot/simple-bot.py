165312aefebbf57635253c8809861d2356dd3499
SVS <v.stepanets@kernel.ua>
2021-12-08 04:45:46 +0200

    Simple Telegram weather bot

---
+++ Lesson7-hw/tg-bot/my-bot/simple-bot.py
@@ -0,0 +1,27 @@
+﻿import requests as rs
+import json
+import telebot
+import os
+import re
+from weather import NewWeatherAPI
+from translate import Translator
+
+bot = telebot.TeleBot(os.getenv("TG_API_KEY"))
+translator= Translator(to_lang="russian")
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+  if message.text == "Погода":
+      bot.send_message(message.from_user.id, "Доброго времени суток, введите, пожалуйста, город. Используйте английский язык.")
+  elif message.text == "/help":
+      bot.send_message(message.from_user.id, "Напишите названия города на английском языке")
+  elif re.match("[A-Z][a-z]", message.text):
+      NewWeatherAPI.SetCity(message.text)
+      NewWeatherAPI.CallAPI()
+      Result = NewWeatherAPI.GetAnswer()
+      bot.send_message(message.from_user.id, " Погода в {0}: {1}\nТемература: {2}°C\nВлажность{3}%\nСкорость ветра: {4} м/с\nВидимость: {5}м".format(message.text, translator.translate(Result["weather"][0]["description"]), round(float(Result["main"]["temp"]) - 273.15), Result["main"]["humidity"], Result["wind"]["speed"], Result["visibility"]))
+  else:
+      bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ Lesson7-hw/tg-bot/my-bot/weather.py
@@ -0,0 +1,47 @@
+﻿import json
+import requests as rs
+import os
+
+class WeatherAPI:
+    __Payload = None
+    __Headers = None
+    URL = "https://api.openweathermap.org/data/2.5/weather"
+    __Answer = None
+
+    def __init__(self):
+        self.__Headers = {
+                            "Content-type": "application/json",
+                            "Accept": "*/*",
+                            "User-Agent" : "Python_Client",
+                            "Content-Encoding": "utf-8"
+                        }
+    def SetCity(self, City):
+        """
+        Setting up HTTP payload.
+        Mandatory attribute <Data>, preferred to be a dict()
+        """
+        Payload = {'q': City, 'appid': os.getenv("OPENWEATHER_API_KEY")}
+        self.__Payload = Payload
+
+    def CallAPI(self):
+        """
+        Make API call to api.openweathermap.org
+        """
+        if self.__Payload:
+            self.__Answer = rs.get(self.URL, params = self.__Payload, headers = self.__Headers)
+            print("API call successful")
+        else:
+            print("No payload in HTTP request")
+            raise Exception("Payload must be setted")
+
+    def GetAnswer(self):
+        """
+        Returns HTTP code of response
+        """
+        return json.loads(self.__Answer.content)
+
+NewWeatherAPI = WeatherAPI()
+#NewWeatherAPI.SetCity("Tomsk")
+#NewWeatherAPI.CallAPI()
+#Ans = NewWeatherAPI.GetAnswer()
+#print(round(float(Ans["main"]["temp"]) - 273.15))
