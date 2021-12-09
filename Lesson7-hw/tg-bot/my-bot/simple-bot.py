5c3bc1b32d3e8dc8152dd74f36b51bbd42770fae
SVS <v.stepanets@kernel.ua>
2021-12-09 14:22:41 +0200

    Add validation for city

--- Lesson7-hw/tg-bot/my-bot/simple-bot.py
+++ Lesson7-hw/tg-bot/my-bot/simple-bot.py
@@ -12,16 +12,20 @@ translator= Translator(to_lang="russian")
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
   if message.text.upper() == "ПОГОДА":
-      bot.send_message(message.from_user.id, "Доброго времени суток, введите, пожалуйста, город. Используйте английский язык.")
+    bot.send_message(message.from_user.id, "Доброго времени суток, введите, пожалуйста, город. Используйте английский язык.")
   elif message.text.upper() == "/HELP":
-      bot.send_message(message.from_user.id, "Напишите: Погода")
+    bot.send_message(message.from_user.id, "Напишите: Погода")
   elif re.match("[A-Z][a-z]", message.text):
+    try:
       NewWeatherAPI.SetCity(message.text)
       NewWeatherAPI.CallAPI()
       Result = NewWeatherAPI.GetAnswer()
+      print(Result["cod"]) #! Debug info
       bot.send_message(message.from_user.id, " Погода в {0}: {1}\nТемература: {2}°C\nВлажность: {3}%\nСкорость ветра: {4} м/с\nВидимость: {5}м".format(message.text, translator.translate(Result["weather"][0]["description"]), round(float(Result["main"]["temp"]) - 273.15), Result["main"]["humidity"], Result["wind"]["speed"], Result["visibility"]))
+    except KeyError:
+      bot.send_message(message.from_user.id, "Нету такого города! Попробуйте ввести другое название.")
   else:
-      bot.send_message(message.from_user.id, "Такой команды нету. Напишите /help.")
+    bot.send_message(message.from_user.id, "Такой команды нету. Напишите /help.")
 
 
 bot.polling(none_stop=True, interval=0)
